a8648530c82a16519bca8d0bb1b5c27f1021eeb0
suggym <162288695+suggym@users.noreply.github.com>
2024-03-14 18:37:56 +1100

    ?

--- routes/index.tsx
+++ routes/index.tsx
@@ -42,3 +42,6 @@ function PostCard(props: { post: Post }) {
     </div>
   );
 }
+<p align="center">
+  <img src="https://capsule-render.vercel.app/api?text=Hey Everyone!🕹️&animation=fadeIn&type=waving&color=gradient&height=100"/>
+</p>
