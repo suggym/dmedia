43a4421ef10feb6c960dca195208f2f51be7bfaf
suggym <162288695+suggym@users.noreply.github.com>
2024-03-14 18:16:43 +1100

    get rid

--- routes/twind.tsx
+++
@@ -1,6 +0,0 @@
-import { tw } from 'https://cdn.skypack.dev/twind'
-
-document.body.innerHTML = `
-  <main class="${tw`h-screen bg-purple-400 flex items-center justify-center`}">
-    <h1 class="${tw`font-bold text(center 5xl white sm:gray-800 md:pink-700)`}">This is Twind!</h1>
-  </main>
